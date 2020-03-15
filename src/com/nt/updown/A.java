0f1d2b46698802bbae3e4869c1ac532b70c46e1a
Rajesh Das <rajeshlonu143@gmail.com>
2020-03-15 23:36:18 +0530

    A.java updatd

--- src/com/nt/updown/A.java
+++ src/com/nt/updown/A.java
@@ -3,7 +3,8 @@ package com.nt.updown;
 public class A {
 	public A() {
 		// TODO Auto-generated constructor stub
-		System.out.println("A.A()");
+		System.out.println("A.A()"); 
+                System.out.println("B.B()");
 	}
 
 }
